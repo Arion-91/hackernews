25b64ad8ef506036622c827f299d2cd981ebda2d
Arion <arion2107@gmail.com>
2018-11-19 14:00:43 +0600

    39

--- src/App.js
+++ src/App.js
@@ -21,12 +21,17 @@ const list = [
 ];
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            list
+        }
+    }
     render() {
         return (
-            <div className="App">
-                {
-                  list.map(function (item) {
-                      return (
+            <div className="App">{
+                this.state.list.map(item =>
                           <div key={item.objectID}>
                               <span>
                                   <a href={item.url}>{item.title}</a>
@@ -34,10 +39,7 @@ class App extends Component {
                               <span>{item.author}</span>
                               <span>{item.num_comments}</span>
                               <span>{item.points}</span>
-                          </div>
-                      );
-                  })
-                }
+                          </div>)}
             </div>
         );
     }
