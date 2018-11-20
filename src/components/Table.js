09f79f8eccdec4dcdd5da09acd38782dcfc25db9
Arion <arion2107@gmail.com>
2018-11-20 10:36:16 +0600

    Преобразование в функциональный стиль

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from './Button';
 
-export default class Table extends Component {
-    render() {
-        const { list, pattern, onDismiss } = this.props;
-        return (
-            <div>
-                {list.filter(isSearched(pattern)).map(item =>
-                    <div key={item.objectID}>
-                                  <span>
-                                      <a href={item.url}>{item.title}</a>
-                                  </span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                        <span>
-                            <Button onClick={() => onDismiss(item.objectID)}>
-                                Отбросить
-                            </Button>
-                        </span>
-                    </div>
-                )}
+const Table = ({list, pattern, onDismiss}) =>
+    <div>
+        {list.filter(isSearched(pattern)).map(item =>
+            <div key={item.objectID}>
+                <span>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                    <Button onClick={() => onDismiss(item.objectID)}>
+                        Отбросить
+                    </Button>
+                </span>
             </div>
-        )
-    }
-}
+        )}
+    </div>;
 
 function isSearched(searchTerm) {
     return function (item) {
@@ -32,3 +26,4 @@ function isSearched(searchTerm) {
     }
 }
 
+export default Table;
