b7ba7b6a1acba5d89684419f16267a64f239fe9e
Arion <arion2107@gmail.com>
2018-11-21 16:11:52 +0600

    Блолкировка изменения состояния и перерисовывания компонента после отправки запроса, когда компонент уже был уничтожен

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ const PARAM_HPP = 'hitsPerPage=';
 // const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
 
 class App extends Component {
+    _isMounted = false;
+
     constructor(props) {
         super(props);
 
@@ -40,8 +42,8 @@ class App extends Component {
 
     fetchSearchTopStories(searchTerm, page = 0) {
         axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-            .then(result => this.setSearchTopStories(result.data))
-            .catch(error => this.setState({ error }));
+            .then(result => this._isMounted && this.setSearchTopStories(result.data))
+            .catch(error => this._isMounted && this.setState({ error }));
     }
 
     onSearchSubmit(event) {
@@ -77,9 +79,15 @@ class App extends Component {
     }
 
     componentDidMount() {
+        this._isMounted = true;
+
         const { searchTerm } = this.state;
         this.setState({ searchKey: searchTerm });
         this.fetchSearchTopStories(searchTerm);
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
     }
 
     onDismiss(id) {
