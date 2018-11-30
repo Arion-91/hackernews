ebba6940de9860c4651cb2b3d9fcc74c1dd55626
Arion <arion2107@gmail.com>
2018-11-30 10:18:46 +0600

    Создали store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,2 +1,5 @@
-// import {applyMiddleware, createStore} from 'redux';
-// import {rootReducer}
+import {applyMiddleware, createStore} from 'redux';
+import {rootReducer} from '../reducers';
+import thunk from 'redux-thunk';
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
