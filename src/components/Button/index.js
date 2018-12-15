ef0f1a992c95bb9910edc87622aeca6c46181eb5
Arion <arion2107@gmail.com>
2018-12-15 11:20:15 +0600

    Опробывал Radium на кнопке. Оказалось что это inline style. Наверно такой подход разумен только для малого количества компонентов или для малого проекта.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Radium from 'radium';
+
+let styles = {
+    base: {
+        border: '0',
+        background: 'transparent',
+        color: 'blue',
+        textAlign: 'inherit',
+        padding: 0,
+        fontSize: 'inherit',
+        cursor: 'pointer',
+
+        ':hover': {
+            color: 'lightblue',
+        },
+    }
+};
 
 const Button = ({onClick, className, children} ) =>
     <button
         onClick={onClick}
         className={className}
         type="button"
+        style={styles.base}
     >
         {children}
     </button>;
@@ -20,4 +38,4 @@ Button.defaultProps = {
     className: ''
 };
 
-export default Button;
+export default Radium(Button);
